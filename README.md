8acc73d9903dca64c5fe91c42c730c4639dcffda
kyu <kyu@spoonradio.co>
2023-04-21 10:42:13 +0900

    readme add

---
+++ README.md
@@ -0,0 +1,16 @@
+# 소피아 core 테스트
+
+### node version
+v18.12.1
+### Install
+```sh
+npm install
+```
+### Start
+``` sh
+npm run dev
+```
+이후 localhost:1234로 접속시 테스트 가능
+### 부연설명
+src/controller/main 에서 스푼에서 사용하는 id, pw를 셋팅
+이후 채팅을 가로챌 라이브 id도 함께 넣어줄 것
--- src/controller/main.ts
+++ src/controller/main.ts
@@ -7,18 +7,21 @@ export const getMain = async (req: Request, res: Response) => {
   try {
     const id = '1012341234';
     const pw = '12341234';
+    const liveId = 12341234;
     const spoon = new SpoonClient('deviceUUID');
     spoon.country = Country.KOREA;
     await spoon.init();
 
     const logonUser = await spoon.login(id, pw, SnsType.PHONE);
-    const req = await spoon.api.lives.access(33482524);
+    const req = await spoon.api.lives.access(liveId);
 
     const [live] = req.res.results;
     const soket = await live.join();
     soket.on(LiveEvent.LIVE_MESSAGE, (data) => {
       console.log('data>', data);
     });
+    // 소켓을 지속적으로 유지하기 임시로 res.send를 사용하지 않음
+    // res.send('ok');
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
